19045455a3d0b43d0d01b743d40f400603c3a039
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2017-03-23 20:10:17 -0600

    fix gallery width calculation. add a bunch of comments

--- src/js/main.js
+++ src/js/main.js
@@ -21,7 +21,7 @@ Site = {
     var _this = this;
 
     if ($('#product-gallery').length) {
-      // debounce gallery
+      // debounce resize event for product gallery 
       clearTimeout(_this.debounceTimer);
 
       _this.debounceTimer = setTimeout(function () {
@@ -68,60 +68,73 @@ Site.Product = {
     var galleryWidth = 0;
     var windowWidth = $(window).width();
 
+    // get total width of gallery items
     $('.product-gallery-item').each(function() {
-      galleryWidth += $(this).width();
+      // using getBoundingClientRect because jQuery width() rounds values
+      galleryWidth += $(this)[0].getBoundingClientRect().width;
     });
 
-    $('#product-gallery-row').width(galleryWidth + 1);
+    //  set gallery width to width of gallery items
+    $('#product-gallery-row').width(galleryWidth);
 
     if (galleryWidth > windowWidth) {
+      // gallery is more than window width
       var galleryContainerWidth = galleryWidth + (galleryWidth - windowWidth);
 
+      // set the gallery container width and horizontally center it
       $('#product-gallery')
-        .width(galleryContainerWidth - 1)
+        .width(galleryContainerWidth)
         .css('left', ((windowWidth / 2) - (galleryContainerWidth / 2)) + 'px');
 
+      // horizontally center gallery in container
       $('#product-gallery-row').css('left', ((galleryContainerWidth / 2) - (galleryWidth / 2)) + 'px');
 
+      // bind dragging
       _this.bindGalleryDrag();
 
     } else {
+      // gallery is less than window width
 
+      // center the gallery container. same width as gallery
       $('#product-gallery')
         .width(galleryWidth)
         .css('left', ((windowWidth / 2) - (galleryWidth / 2)) + 'px');
 
+      // 0 the gallery position in its container
       $('#product-gallery-row').css({
         'left' : '0'
       });
 
+      // unbind dragging
       _this.unbindGalleryDrag();
     }
 
   },
 
   bindGalleryDrag: function() {
+    // bind the drag and change cursor to arrows
     $('#product-gallery-row').pep({
       axis: 'x',
       constrainTo: 'parent',
       place: false,
       useCSSTranslation: false
     }).css('cursor', 'ew-resize');
-
   },
 
   unbindGalleryDrag: function() {
+    // unbind the drag and change cursor to default
     $.pep.unbind($('#product-gallery-row'));
     $('#product-gallery-row').css('cursor', 'default');
   },
 
   selectCallback: function(variant, selector) {
     if (variant && variant.available === true) {
+      // variant exists. hide out-of-stock notice and enable button
       $('#product-add-holder').removeClass('out-of-stock');
       $('#add').removeAttr('disabled');
     }
     else {
-      // variant doesn't exist
+      // variant doesn't exist. show out-of-stock notice and disable button
       $('#product-add-holder').addClass('out-of-stock');
       $('#add').attr('disabled', 'disabled');
     }
@@ -131,15 +144,20 @@ Site.Product = {
     var _this = this;
     var productJson = JSON.parse($('#product-info').attr('data-product'));
 
+    // replaces variant select with multiple variant selects.
+    // handles select callback
     new Shopify.OptionSelectors("product-select", {
       product: productJson,
       onVariantSelected: _this.selectCallback
     });
 
+    // add some extra padding-top to add-to-cart button
+    // container when we have variant selects.
     $('#product-add-holder').addClass('padding-top-small');
   },
 
   pickRelated: function() {
+    // keeps random 4 related products and removes others
     $('.related-products-item').pick(4);
 
     $('#related-products').removeClass('u-hidden');
